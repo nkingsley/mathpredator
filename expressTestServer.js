91b09092b7db5cbb727c3a7a615da3cb0a99da3d
Noah Cason-Zesiger <noahcason-zesiger@Noahs-MacBook-Air.local>
2013-12-16 12:05:28 -0800

    sockets now join teams, know about the team numbers, basic structure coming into place.  No major bugs

--- expressTestServer.js
+++ expressTestServer.js
@@ -12,7 +12,7 @@ var Team = function(id){
   this.score = 0;
   this.id = id;
 }
-var teams = [new Team(1), new Team(2)];
+var teams = [new Team(0), new Team(1)];
 var playerLevels = {};
 server.listen(process.env.PORT || 5000);
 
@@ -22,39 +22,41 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
 	socket.join(id++);
-  socket.emit('initialize', id);
   playerLevels[id] = 0;
   var team;
-	if (teams[0].players.length > team[1].players.length){
-	  team = team[0].id;
+  var otherTeam;
+	if (teams[1].players.length > teams[0].players.length){
+	  team = teams[0].id;
     teams[0].players.push(id);
+    otherTeam = teams[1].id;
 	} else {
-	  team = team[1].id;
+	  team = teams[1].id;
     teams[1].players.push(id);
+    otherTeam = teams[0].id;
 	}
+  socket.emit('initialize', {id:id,team:team,otherTeam:otherTeam});
   socket.join('team' + team);
-	io.sockets.emit('newConnect', [team[0],team[1]] );
+	io.sockets.emit('newConnect', [teams[0],teams[1]] );
   socket.on('boom', function (data) {
     playerLevels[data.id]++;
 	  var team = data.team;
-    teams[team].score ++;
+    console.log('team->' + team);
+    teams[team].score++;
     var otherTeam = data.otherTeam;
     if (data.hit){
-      io.sockets.in(data.hit).emit('hit', data.id);
+      io.sockets.emit('hit', {biter: data.id,bitten:data.hit});
       playerLevels[data.hit]--;
       teams[otherTeam].score--;
     }
-	  io.sockets.emit('bam', data.id );
-  	if (team[team].score === 10){
+  	if (teams[team].score === 10){
       //game over
-  	  team[team].score = 0;
-  	  team[otherTeam].score = 0;
+  	  teams[team].score = 0;
+  	  teams[otherTeam].score = 0;
       io.sockets.in('team' + team).emit('gameOver',"WIN");
   	  io.sockets.in('team' + otherTeam).emit('gameOver',"LOSE");
   	} else {
       //let everyone know this player is safe for the round
       io.sockets.emit('safe',data.id);
-      
     }
 
   });
